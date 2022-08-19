09ea64e4ef85af37096b1a593d8899f6d9ceca2b
pedro1287 <102392245+pedro1287@users.noreply.github.com>
2022-08-19 17:48:43 -0600

    Update auth.py

--- plugins/file2free/auth.py
+++ plugins/file2free/auth.py
@@ -3,7 +3,7 @@ import requests
 
 F2FAUTH = 'https://raw.githubusercontent.com/JAGB2021/database/blob/main/f2fauth.txt'
 
-def auth(JAGB2021):
+def auth(username):
     list = []
     try:
         resp = requests.get(F2FAUTH)
